64a690163ce3f59cc07d098b3f254d75539380d8
nadsay <nadsatyyy@gmail.com>
2023-08-16 00:26:52 +0300

    Домашка 6, Задание 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1 \n");
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(i);
+        }
+    }
+}
