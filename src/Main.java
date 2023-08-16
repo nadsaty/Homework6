5493881105d134944f06e953e343ff92b6b5cbd7
nadsay <nadsatyyy@gmail.com>
2023-08-16 19:19:15 +0300

    Домашка 6, Задание 4

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
     public static void task1 () {
         System.out.println("\n Задача 1 \n Числа по порядку: \n");
@@ -22,6 +23,13 @@ public class Main {
     public static void task3 () {
         System.out.println("\n Задача 3 \n Четные числа от 0 до 17: \n");
         for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task4 () {
+        System.out.println("\n Задача 4 \n Числа в обратном порядке от 10 до -10: \n");
+        for (int i = 10; i >= -10; i = i - 1) {
             System.out.println(i);
         }
     }
